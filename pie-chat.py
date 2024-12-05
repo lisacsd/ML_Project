0571ccb4157a294a7e11748bacf0b99b99a89b84
Inesmeh <125276537+charlottines@users.noreply.github.com>
2024-12-05 14:28:54 +0000

    Update pie-chat.py

---
+++ pie-chat.py
@@ -0,0 +1,19 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+
+data = pd.read_excel("./Student_Mental_Health_Cleaned (1).xlsx")
+
+
+mental_pb = ['Depression', 'Anxiety', 'Panic Attack']
+pb_count = {pb: data[pb].value_counts()['Yes'] for pb in mental_pb}
+
+
+labels = list(pb_count.keys())
+sizes = list(pb_count.values())
+
+
+plt.figure(figsize=(6, 6))
+plt.pie(sizes, labels=labels, autopct='%1.1f%%', startangle=140)
+plt.title("Which mental health is present the most?")
+plt.show()
