8320a5e3f35842ea7c0b2851b170e05718b38232
inesmeh <in.mehadhebi@gmail.com>
2024-12-06 13:07:59 +0000

    same

--- preprocessing.py
+++ preprocessing.py
@@ -5,10 +5,10 @@ from sklearn.compose import ColumnTransformer
 file_path = 'Student_Mental_Health_Cleaned (1).xlsx'
 data = pd.read_excel(file_path)
 
-# Drop unnecessary columns
+
 data = data.drop(columns=['Timestamp'])
 
-# Define numerical and categorical features
+
 numerical_features = ['Age']
 categorical_features = ['Gender', 'Course', 'Year of Study', 'CGPA',
                         'Marital Status', 'Depression', 'Anxiety', 'Panic Attack', 'Specialist Treatment']
@@ -17,25 +17,24 @@ categorical_features = ['Gender', 'Course', 'Year of Study', 'CGPA',
 data[numerical_features] = data[numerical_features].fillna(data[numerical_features].mean())
 data[categorical_features] = data[categorical_features].fillna("Unknown")
 
-# Define the preprocessor
+
 preprocessor = ColumnTransformer(
     transformers=[
         ('num', StandardScaler(), numerical_features),
         ('cat', OneHotEncoder(drop='first', sparse_output=False), categorical_features)
     ])
 
-# Fit and transform the data
+
 preprocessed_data = preprocessor.fit_transform(data)
 
-# Get column names for the preprocessed data
+
 num_col_names = numerical_features
 cat_col_names = preprocessor.named_transformers_['cat'].get_feature_names_out(categorical_features)
 all_col_names = list(num_col_names) + list(cat_col_names)
 
-# Create a DataFrame for the preprocessed data
 preprocessed_df = pd.DataFrame(preprocessed_data, columns=all_col_names)
 
-# Save the DataFrame as an Excel file
+
 output_file = "Preprocessed_Student_Mental_Health.xlsx"
 preprocessed_df.to_excel(output_file, index=False)
 
--- ver_preprocess_pie.py
+++ ver_preprocess_pie.py
@@ -1,21 +1,20 @@
-
 import pandas as pd
 import matplotlib.pyplot as plt
 
-# Load your data (ensure the file path is correct)
-data = pd.read_excel("./Preprocessed_Student_Mental_Health.xlsx")  # Update with the correct file path or format
 
-# Define the mental health problems of interest
+data = pd.read_excel("./Preprocessed_Student_Mental_Health.xlsx")
+
+
 mental_pb = ['Depression_Yes', 'Anxiety_Yes', 'Panic Attack_Yes']
 
-# Count the occurrences of "Yes" for each mental health problem
+
 pb_count = {pb: data[pb].sum() for pb in mental_pb}
 
-# Extract labels and sizes for the pie chart
+
 labels = list(pb_count.keys())
 sizes = list(pb_count.values())
 
-# Plot the pie chart
+
 plt.figure(figsize=(6, 6))
 plt.pie(sizes, labels=labels, autopct='%1.1f%%', startangle=140)
 plt.title("Which mental health issue is present the most?")
