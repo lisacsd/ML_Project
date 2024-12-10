f71662e0469a7d5268d1505f5832523d29696ed8
Lisa <lisa.bail@efrei.net>
2024-12-10 12:16:03 +0000

    start of summ stats

--- preprocessing.py
+++ preprocessing.py
@@ -28,23 +28,3 @@ preprocessor = ColumnTransformer(
     remainder='passthrough'
 )
 
-
-preprocessed_data = preprocessor.fit_transform(data)
-
-
-cat_col_names = preprocessor.named_transformers_['cat'].get_feature_names_out(categorical_features)
-
-
-all_feature_names = list(cat_col_names) + numerical_features
-
-
-preprocessed_df = pd.DataFrame(preprocessed_data, columns=all_feature_names)
-
-columns_order = ['Age'] + [col for col in preprocessed_df.columns if col != 'Age']
-preprocessed_df = preprocessed_df[columns_order]
-
-
-output_file = "Preprocessed_Student_Mental_Health.xlsx"
-preprocessed_df.to_excel(output_file, index=False)
-
-print(f"file saved: {output_file}")
