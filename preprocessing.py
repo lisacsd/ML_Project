a90dff2c7ffea6f457aa805ea5e4504f763d4b43
inesmeh <in.mehadhebi@gmail.com>
2024-12-13 12:33:26 +0000

    preprocess

--- preprocessing.py
+++ preprocessing.py
@@ -3,9 +3,7 @@ from sklearn.preprocessing import OneHotEncoder
 from sklearn.compose import ColumnTransformer
 
 path = 'Student_Mental_Health_Cleaned.xlsx'
-
 df = pd.read_excel(path)
-
 df = df.drop(columns=['Timestamp'], errors='ignore')
 
 categories = ['Gender', 'Course', 'Year of Study', 'CGPA',
@@ -23,13 +21,9 @@ transformer = ColumnTransformer(
 )
 
 transformed_data = transformer.fit_transform(df)
-
 encoded_columns = transformer.named_transformers_['encoder'].get_feature_names_out(categories)
-
 all_columns = list(encoded_columns) + numerics
-
 final_df = pd.DataFrame(transformed_data, columns=all_columns)
-
 ordered_columns = ['Age'] + [col for col in final_df.columns if col != 'Age']
 final_df = final_df[ordered_columns]
 
